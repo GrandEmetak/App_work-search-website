b4cdc556665cb77377b89838c9c6861c3f807cae
GrandEmetak <JekHamond@tutanota.com>
2023-01-15 18:29:27 +0500

    fix: deleted comments -LogoutServlet

--- src/main/java/com/worksearch/website/servlet/LogoutServlet.java
+++ src/main/java/com/worksearch/website/servlet/LogoutServlet.java
@@ -9,23 +9,12 @@ import java.io.IOException;
 
 /**
  * -@WebServlet(urlPatterns = "/logout.do")
- * 1. HttpSession [#6869 #209565]
- * Уровень : 3. Мидл Категория : 3.2. Servlet JSP Топик : 3.2.6. Filter, Security
- * ВАЖНО! установлена аннотация @WebServlet вместо web.xml
- * Добавим сервлет LogoutServlet,
- * который будет обрабатывать этот запрос
- * <c:if test="${user != null}">
- *     <li class="nav-item">
- *         <a class="nav-link" href="<%=request.getContextPath()%>/logout.do">Выйти</a>
- *     </li>
- * </c:if>
- * следующим образом:
- * @author SlartiBartFast-art
  * @since 20.10.21
  */
 public class LogoutServlet extends HttpServlet {
+
     /**
-     * Код сервлета достаточно прост, мы получаем сессию в следующей строке:
+     * мы получаем сессию в следующей строке:
      * HttpSession session = req.getSession();
      * а потом у этой сессии вызываем метод invalidate() который очищает
      * сессию и удаляет все добавленные ранее в нее атрибуты.
